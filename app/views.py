20bbb4feed9677626d7f6eea08814f4e245407ad
Tanvir Reza <tanvir.reza.abc@gmail.com>
2025-01-26 17:33:34 +0600

    setup mail with attachment

--- app/views.py
+++ app/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+# HTTPResponse is a class that takes a string as an argument and returns an HTTP response object.
+from django.http import HttpResponse
+
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.conf import settings
 
 # Create your views here.
+def index(request):
+    subject = 'Hello, World!'
+    html_content = render_to_string('my_mail.html')
+    email_from = settings.DEFAULT_FROM_EMAIL
+    email_to = 'henkamaroon@goeschman.com'
+    msg = EmailMultiAlternatives(subject, html_content, email_from, [email_to])
+    msg.attach_alternative(html_content, "text/html")  
+
+    msg.send()
+
+    if msg:
+        return HttpResponse('Email sent!')
+    else:
+        return HttpResponse('Email not sent!')
--- core/settings.py
+++ core/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,6 +139,15 @@ CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
 CELERY_TIMEZONE = 'Asia/Dhaka'
+
+
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+EMAIL_USE_TLS = True
 
 # Celery Beat Schedule
 # CELERY_BEAT_SCHEDULE = {
